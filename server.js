0f8b1ecbf3fc11e471acc02e80c3d439983de715
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2023-02-04 18:25:42 -0800

    finished part 2

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [
+  {
+    temperature: "72",
+    date: "02/04/2023",
+    user_response: "Hello Udacity",
+  },
+];
 
 // Require Express to run server and routes
 const express = require("express");
@@ -14,9 +20,23 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require("cors");
+const { response } = require("express");
 app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
+
+// Get Route returns projectData object in your server code
+app.get("/projectData", (request, response) => {
+  response.send(projectData);
+});
+
+// add a Post route that adds incoming data to projectData
+app.post("/addData", (request, response) => {
+  console.log(request);
+  console.log(response);
+  response.send(request.body);
+  projectData.push({ temperature: "90", date: "02/10/2023", user_response: "Bye!" });
+});
 
 // Setup Server
 const port = 8080;
